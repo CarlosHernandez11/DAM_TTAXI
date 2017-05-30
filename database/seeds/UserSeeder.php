405769185643d42d563a6ae85e6664ddb0277ac2
Carlos Eduardo Hernandez Velador <caedhernandezve@ittepic.edu.mx>
2017-05-30 02:27:27 -0600

    Agramos mas datos de prueba

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -19,5 +19,52 @@ class UserSeeder extends Seeder
             'password' => "123456",
             'facebook' => 'https://www.facebook.com/Jhonee.Jhonee'
         ]);
+        User::create([
+            'name' => 'Carlos Eduardo Hernandez Velador',
+            'email' => 'caedhernandezve@ittepic.edu.mx',
+            'cellphone' => '3111566022',
+            'password' => "123456",
+            'facebook' => 'https://www.facebook.com/Jhonee.Jhonee'
+        ]);
+
+        User::create([
+            'name' => 'Jonathan Camarena Ortega',
+            'email' => 'jocamarenaor@ittepic.edu.mx',
+            'cellphone' => '3111956909',
+            'password' => "123456",
+            'facebook' => 'https://www.facebook.com/Jhonee.Jhonee2'
+        ]);
+
+        User::create([
+            'name' => 'Alexis Hermosillo Becerra',
+            'email' => 'alexhermo@ittepic.edu.mx',
+            'cellphone' => '3112565164',
+            'password' => "123456",
+            'facebook' => 'https://www.facebook.com/Jhonee.Jhonee3'
+        ]);
+
+        User::create([
+            'name' => 'Tony Valencia Torres',
+            'email' => 'tony@ittepic.edu.mx',
+            'cellphone' => '3112344321',
+            'password' => "123456",
+            'facebook' => 'https://www.facebook.com/Jhonee.Jhonee4'
+        ]);
+
+        User::create([
+            'name' => 'Minerva Anahi Sanchez Pacheco',
+            'email' => 'minerva@ittepic.edu.mx',
+            'cellphone' => '3111104532',
+            'password' => "123456",
+            'facebook' => 'https://www.facebook.com/Jhonee.Jhonee5'
+        ]);
+
+        User::create([
+            'name' => 'Mizael Chacon Lopez',
+            'email' => 'miza@ittepic.edu.mx',
+            'cellphone' => '3112343211',
+            'password' => "123456",
+            'facebook' => 'https://www.facebook.com/Jhonee.Jhonee6'
+        ]);
     }
 }
